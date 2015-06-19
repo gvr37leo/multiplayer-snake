def855f7b3ef8d08b1f3379228745fd9c165ff5b
Paul Boon <gvr37.p.l.boon@gmail.com>
2015-06-19 15:07:28 +0200

    finished part 2 of nodejs tutorial

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require("express"),
 app = express(),
 server = require("http").createServer(app),
-io = require("socket.io").listen(server);
+io = require("socket.io").listen(server),
+nicknames = [];
 server.listen(8000);
 
 app.get('/',function(req,res){
@@ -9,7 +10,26 @@ app.get('/',function(req,res){
 });
 
 io.sockets.on("connection",function(socket){
-	socket.on("send-message",function(data){
-		io.sockets.emit("new message",data);
+	socket.on("new user",function(data,callback){
+		if(nicknames.indexOf(data) != -1){
+			callback(false);
+		} else{
+			callback(true);
+			socket.nickname = data;
+			nicknames.push(socket.nickname);
+			io.sockets.emit("usernames",nicknames);
+		}
 	})
+
+	socket.on("send-message",function(data){
+		io.sockets.emit("new message",socket.nickname + ":" + data);
+	})
+
+	socket.on("disconnect",function(data){
+		if(!socket.nickname){
+			return;
+		}
+		nicknames.splice(nicknames.indexOf(socket.nickname),1);
+		io.sockets.emit("usernames",nicknames);
+	});
 });
